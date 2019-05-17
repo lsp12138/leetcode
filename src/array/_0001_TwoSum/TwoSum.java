c64958bad60a873a802547c6beb71057fa8cbc30
liusp70 <liusp70@chinaunicom.cn>
2019-05-17 23:55:09 +0800

    test

---
+++ src/array/_0001_TwoSum/TwoSum.java
@@ -0,0 +1,17 @@
+package array._0001_TwoSum;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
+
+public class TwoSum {
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello");
+
+    }
+
+}
