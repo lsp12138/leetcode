502a731bfdfb22dee7100a06d7add4e7503b9eb2
liusp70 <liusp70@chinaunicom.cn>
2019-06-05 13:46:35 +0800

    0016 done

--- src/array/_0016_3SumClosest/main.go
+++ src/array/_0016_3SumClosest/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"sort"
 	"fmt"
+	"math"
+	"sort"
 )
 
 func main() {
@@ -12,6 +13,32 @@ func main() {
 }
 
 func threeSumClosest(nums []int, target int) int {
+	// 升序
 	sort.Ints(nums)
-	
+	result := math.MaxInt64
+	// 固定第一个数
+	for first := range nums {
+		// 跳过重复的数
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// 一前一后双指针指向另外两个数
+		second := first + 1
+		third := len(nums) - 1
+		for second < third {
+			currentResult := nums[first] + nums[second] + nums[third]
+			if math.Abs(float64(target-result)) > math.Abs(float64(target-currentResult)) {
+				result = currentResult
+			}
+			switch {
+			case currentResult < target:
+				second++
+			case currentResult > target:
+				third--
+			default:
+				return target
+			}
+		}
+	}
+	return result
 }
