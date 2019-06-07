b1b132c6c446147d61e4f3266478cc80203afd73
liusp70 <liusp70@chinaunicom.cn>
2019-06-07 15:11:10 +0800

    0011 done

--- src/array/_0011_ContainerWithMostWater/main.go
+++ src/array/_0011_ContainerWithMostWater/main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("h2222")
+	height := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
+	fmt.Println(maxArea(height))
+}
+
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	area := 0
+	areaTemp := 0
+	for left < right {
+		if height[left] < height[right] {
+			areaTemp = height[left] * (right - left)
+			left++
+		} else {
+			areaTemp = height[right] * (right - left)
+			right--
+		}
+		if area < areaTemp {
+			area = areaTemp
+		}
+	}
+	return area
 }
