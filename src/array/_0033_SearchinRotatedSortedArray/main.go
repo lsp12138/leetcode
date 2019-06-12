99136f2a153a914ac325c434461fc57cb12dc040
lsp <550372760@qq.com>
2019-06-12 23:39:53 +0800

    0033 not done

--- src/array/_0033_SearchinRotatedSortedArray/main.go
+++ src/array/_0033_SearchinRotatedSortedArray/main.go
@@ -9,58 +9,32 @@ func main() {
 }
 
 func search(nums []int, target int) int {
-	if len(nums) == 0 {
-		return -1
-	}
-	rotateIndex := findRotateIndex(nums)
-	left := 0
-	right := len(nums) - 1
-	if nums[rotateIndex] == target {
-		return rotateIndex
-	}
-	if nums[left] == target {
-		return left
-	}
-	if nums[right] == target {
-		return right
-	}
-	if nums[left] > target {
-		return binarySearch(nums, rotateIndex, right, target)
-	}
-	return binarySearch(nums, left, rotateIndex, target)
 
 }
 
-// 找到旋转数组中最小的数的下标
-func findRotateIndex(nums []int) int {
-	left := 0
-	right := len(nums) - 1
-	mid := (left + right) / 2
-	for left < right {
-		if nums[mid] > nums[mid+1] {
-			return mid + 1
-		}
-		if nums[mid] > nums[left] {
-			left = mid + 1
-		} else {
-			right = mid - 1
-		}
-	}
-	return 0
-}
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         if (nums == null || nums.length == 0) return -1;
+//         int left = 0;
+//         int right = nums.length - 1;
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] > nums[right]) left = mid + 1;
+//             else right = mid;
+//         }
+//         //System.out.println(left);
+//         int split_t = left;
+//         left = 0;
+//         right = nums.length - 1;
+//         if (nums[split_t] <= target && target <= nums[right]) left = split_t;
+//         else right = split_t;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             if (nums[mid] == target) return mid;
+//             else if (nums[mid] > target) right = mid - 1;
+//             else left = mid + 1;
+//         }
+//         return -1;
 
-// 二分查找
-func binarySearch(nums []int, left int, right int, target int) int {
-	for left < right {
-		mid := (left + right) / 2
-		if nums[mid] == target {
-			return mid
-		}
-		if nums[mid] > target {
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-	return -1
-}
+//     }
+// }
